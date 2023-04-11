e8df17934df14f667288d033a9d0d414a55a7494
Andrés Howard <ahowardm@gmail.com>
2023-04-11 11:18:21 -0400

    ejercici de clase

---
+++ main.py
@@ -0,0 +1,12 @@
+from linked_list import LinkedList
+
+my_list = LinkedList()
+my_list.print_elements()
+my_list.add(1)
+my_list.add(2)
+my_list.add(3)
+my_list.add(3)
+my_list.add(3)
+my_list.add(3)
+my_list.add(3)
+my_list.print_elements()
